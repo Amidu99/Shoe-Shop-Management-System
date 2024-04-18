a3a15f9b1387f339f990be9a15b21dbca2318c4b
Amidu99 <amidushamika@gmail.com>
2024-04-18 19:35:43 +0530

    add customer controller

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Customer.java
@@ -0,0 +1,10 @@
+package lk.ijse.HelloShoesBE.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class Customer {
+
+}
