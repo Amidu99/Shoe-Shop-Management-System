e885d2a31c45a0e9c96d87ba73a2c2f1f4ebb73c
Amidu99 <amidushamika@gmail.com>
2024-04-18 15:59:11 +0530

    init the project

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/HelloShoesBEApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.HelloShoesBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesBEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShoesBEApplication.class, args);
+	}
+
+}
---
+++ HelloShoesBE/src/test/java/lk/ijse/HelloShoesBE/HelloShoesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.HelloShoesBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
