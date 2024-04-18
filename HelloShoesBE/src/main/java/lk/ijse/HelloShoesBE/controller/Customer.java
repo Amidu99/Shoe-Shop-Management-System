05727dd78e9befb7fcdb1c78b54e996d86960e40
Amidu99 <amidushamika@gmail.com>
2024-04-18 19:37:49 +0530

    add customer healthTest

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Customer.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Customer.java
@@ -1,10 +1,15 @@
 package lk.ijse.HelloShoesBE.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class Customer {
-
+    @GetMapping("/health")
+    public String healthTest(){
+        System.out.println("Customer Health Test Passed.");
+        return "Customer Health Test Passed.";
+    }
 }
