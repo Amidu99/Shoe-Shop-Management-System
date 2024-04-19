baebafd2648f2e2e174561b6d6025ea6422a362e
Amidu99 <amidushamika@gmail.com>
2024-04-19 10:31:04 +0530

    add CustomerRepo

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/CustomerRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.HelloShoesBE.repo;
+
+import lk.ijse.HelloShoesBE.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, String> {
+}
