a2b91a8cca20e4206760d95833004fb87a9bfec7
Amidu99 <amidushamika@gmail.com>
2024-04-19 10:34:54 +0530

    add CustomerServiceIMPL

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/impl/CustomerServiceIMPL.java
@@ -0,0 +1,32 @@
+package lk.ijse.HelloShoesBE.service.impl;
+
+import lk.ijse.HelloShoesBE.dto.CustomerDTO;
+import lk.ijse.HelloShoesBE.service.CustomerService;
+import java.util.List;
+
+public class CustomerServiceIMPL implements CustomerService {
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(String customerCode) {
+
+    }
+
+    @Override
+    public CustomerDTO getSelectedCustomer(String customerCode) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(String customerCode, CustomerDTO customerDTO) {
+
+    }
+}
