4b7a6ee08906f4b6baeeb3885419792bfb211c92
Amidu99 <amidushamika@gmail.com>
2024-04-25 14:15:18 +0530

    add ModelMapper

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/HelloShoesBEApplication.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/HelloShoesBEApplication.java
@@ -1,10 +1,18 @@
 package lk.ijse.HelloShoesBE;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class HelloShoesBEApplication {
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloShoesBEApplication.class, args);
