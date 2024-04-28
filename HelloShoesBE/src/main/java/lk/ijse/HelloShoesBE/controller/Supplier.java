6c10a94475cf58589dbf5128949509b6be3c62a9
Amidu99 <amidushamika@gmail.com>
2024-04-28 18:22:40 +0530

    add validate supplier with custom exceptions

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Supplier.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Supplier.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api/v1/supplier")
@@ -22,10 +23,24 @@ public class Supplier {
     @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveSupplier(@RequestBody SupplierDTO supplierDTO) {
         try {
+            validateSupplier(supplierDTO);
             supplierService.saveSupplier(supplierDTO);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    private void validateSupplier(SupplierDTO supplierDTO) {
+        if (!Pattern.compile("^[S]-\\d{4}$").matcher(supplierDTO.getSupplierCode()).matches()) {
+            throw new RuntimeException("Invalid Supplier Code.");
+        }
+        if (!Pattern.compile("^[A-Za-z\\s]{3,}$").matcher(supplierDTO.getSupplierName()).matches()) {
+            throw new RuntimeException("Invalid Supplier Name.");
+        }
+        if (!Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$").matcher(supplierDTO.getEmail()).matches()) {
+            throw new RuntimeException("Invalid Supplier Email.");
+        }
+        System.out.println("Supplier validated.");
     }
 }
