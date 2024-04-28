9175070a63beb4106cb7dcbd3c22d52653698939
Amidu99 <amidushamika@gmail.com>
2024-04-28 17:35:39 +0530

    add SupplierRepo

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/SupplierRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.HelloShoesBE.repo;
+
+import lk.ijse.HelloShoesBE.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepo extends JpaRepository<Supplier, String> {
+}
