b503ab11b3a33cd3cac999bea7d854e8c7610f16
Amidu99 <amidushamika@gmail.com>
2024-04-30 19:09:48 +0530

    add employee healthTest

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Employee.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Employee.java
@@ -2,8 +2,7 @@ package lk.ijse.HelloShoesBE.controller;
 
 import lk.ijse.HelloShoesBE.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/employee")
@@ -11,5 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Employee {
     private final EmployeeService employeeService;
 
-
+    @GetMapping("/health")
+    public String healthTest(){
+        System.out.println("Employee Health Test Passed.");
+        return "Employee Health Test Passed.";
+    }
 }
