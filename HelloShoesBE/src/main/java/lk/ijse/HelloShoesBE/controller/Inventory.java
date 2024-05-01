ea57946d738f9bb5f2ebbcd1e4929eee50cfb4c0
Amidu99 <amidushamika@gmail.com>
2024-05-01 15:50:22 +0530

    add inventory healthTest

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Inventory.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Inventory.java
@@ -2,6 +2,7 @@ package lk.ijse.HelloShoesBE.controller;
 
 import lk.ijse.HelloShoesBE.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Inventory {
     private final InventoryService inventoryService;
 
+    @GetMapping("/health")
+    public String healthTest(){
+        System.out.println("Inventory Health Test Passed.");
+        return "Inventory Health Test Passed.";
+    }
 }
