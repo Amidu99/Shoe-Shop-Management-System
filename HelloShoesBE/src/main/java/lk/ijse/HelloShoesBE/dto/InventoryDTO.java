3500a7d58b5b751b0416db2f5dae57689d195ad9
Amidu99 <amidushamika@gmail.com>
2024-05-05 22:11:36 +0530

    update InventoryDTO

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/dto/InventoryDTO.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/dto/InventoryDTO.java
@@ -12,12 +12,10 @@ public class InventoryDTO implements SuperDTO {
     private String itemDesc;
     private String itemPic;
     private String category;
-    private int size;
     private String supplierCode;
     private String supplierName;
     private double buyPrice;
     private double sellPrice;
     private double profit;
     private double profitMargin;
-    private String status;
 }
