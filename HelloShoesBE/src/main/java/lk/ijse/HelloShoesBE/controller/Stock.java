6f4ffd3501f9ece9e9fac359f28dea9e16a9b75d
Amidu99 <amidushamika@gmail.com>
2024-05-06 09:43:15 +0530

    add healthTest

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Stock.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Stock.java
@@ -1,6 +1,8 @@
 package lk.ijse.HelloShoesBE.controller;
 
+import lk.ijse.HelloShoesBE.service.StockService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/stock")
 @RequiredArgsConstructor
 public class Stock {
+    private final StockService stockService;
 
+    @GetMapping("/health")
+    public String healthTest(){
+        System.out.println("Stock Health Test Passed.");
+        return "Stock Health Test Passed.";
+    }
 }
