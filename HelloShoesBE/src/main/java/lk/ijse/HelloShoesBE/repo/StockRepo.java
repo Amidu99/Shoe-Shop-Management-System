08fd122f3aa37b52f4d3518cfebf929e0aefa313
Amidu99 <amidushamika@gmail.com>
2024-05-06 08:43:12 +0530

    add StockRepo

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/StockRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.HelloShoesBE.repo;
+
+import lk.ijse.HelloShoesBE.entity.SupplierInventories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepo extends JpaRepository<SupplierInventories, String> {
+}
