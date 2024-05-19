aaf4d59f250768a11a111f617089cf3ccb46a9a1
Amidu99 <amidushamika@gmail.com>
2024-05-19 22:00:34 +0530

    add SignIn

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/secureAndResponse/secure/SignIn.java
@@ -0,0 +1,15 @@
+package lk.ijse.HelloShoesBE.secureAndResponse.secure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignIn {
+    private String email;
+    private String password;
+}
