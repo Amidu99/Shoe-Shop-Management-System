46eee4c4ba49c94e4a0f1a236639455fee05ea3a
Amidu99 <amidushamika@gmail.com>
2024-05-19 21:29:42 +0530

    update AppConfig

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/config/AppConfig.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/config/AppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+    // RestTemplate Configuration
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    public RestTemplate restTemplate() { return new RestTemplate(); }
+    // CORS Configuration
+    @Bean
+    public CORSConfig corsConfig() { return new CORSConfig(); }
 }
