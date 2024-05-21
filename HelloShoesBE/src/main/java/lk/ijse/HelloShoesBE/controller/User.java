d4fedca74f5bf8bda3ed33619715b0ddb58a2aeb
Amidu99 <amidushamika@gmail.com>
2024-05-21 10:54:21 +0530

    add User healthTest

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/User.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/User.java
@@ -1,7 +1,9 @@
 package lk.ijse.HelloShoesBE.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import lk.ijse.HelloShoesBE.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class User {
     private final UserService userService;
 
+    @GetMapping("/health")
+    @RolesAllowed({"ADMIN", "USER"})
+    public String healthTest(){
+        System.out.println("User Health Test Passed.");
+        return "User Health Test Passed.";
+    }
 }
