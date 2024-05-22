cd64508a40e9231cb7994ef186f30b6dc1165f47
Amidu99 <amidushamika@gmail.com>
2024-05-22 08:14:12 +0530

    add sale controller

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/controller/Sale.java
@@ -0,0 +1,12 @@
+package lk.ijse.HelloShoesBE.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/sale")
+@RequiredArgsConstructor
+public class Sale {
+
+}
