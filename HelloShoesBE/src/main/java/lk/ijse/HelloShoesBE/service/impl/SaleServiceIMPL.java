c1a70bd0e478bd537fbaa9aa6d0ed77e70bec9b4
Amidu99 <amidushamika@gmail.com>
2024-05-22 08:20:59 +0530

    add SaleServiceIMPL

---
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/impl/SaleServiceIMPL.java
@@ -0,0 +1,12 @@
+package lk.ijse.HelloShoesBE.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.HelloShoesBE.service.SaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SaleServiceIMPL implements SaleService {
+}
