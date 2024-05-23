b35bcf19abb6783feed21fab91a54e7da31c1c31
Amidu99 <amidushamika@gmail.com>
2024-05-24 01:10:47 +0530

    update SaleService

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/SaleService.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/SaleService.java
@@ -1,4 +1,14 @@
 package lk.ijse.HelloShoesBE.service;
 
+import lk.ijse.HelloShoesBE.dto.SaleDTO;
+import java.util.List;
+import java.util.Optional;
+
 public interface SaleService {
+    SaleDTO saveSale(SaleDTO saleDTO);
+    boolean existsByOrderCode(String oderCode);
+    Optional<SaleDTO> getOrderByOrderCode(String oderCode);
+    List<SaleDTO> getAllSales();
+    String getLastOrderCode();
+    void updateSale(SaleDTO saleDTO);
 }
