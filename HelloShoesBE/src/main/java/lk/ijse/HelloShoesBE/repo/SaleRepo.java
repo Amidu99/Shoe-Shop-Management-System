cf6f3730fc058fb29c1e5b7774926cf87d684134
Amidu99 <amidushamika@gmail.com>
2024-05-24 01:01:11 +0530

    update SaleRepo

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/SaleRepo.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/SaleRepo.java
@@ -1,7 +1,18 @@
 package lk.ijse.HelloShoesBE.repo;
 
+import lk.ijse.HelloShoesBE.dto.SaleDTO;
 import lk.ijse.HelloShoesBE.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Optional;
 
 public interface SaleRepo extends JpaRepository<Sale, String> {
+    boolean existsByOrderCode(String oderCode);
+    @Query("SELECT NEW lk.ijse.HelloShoesBE.dto.SaleDTO(s.orderCode, s.customerName, s.totalPrice, s.date, s.payMethod, s.addedPoints, s.cashierName, s.user.userCode, s.customer.customerCode) FROM Sale s WHERE s.orderCode = :orderCode")
+    Optional<SaleDTO> findSaleInfoByOrderCode(String orderCode);
+    @Query("SELECT NEW lk.ijse.HelloShoesBE.dto.SaleDTO(s.orderCode, s.customerName, s.totalPrice, s.date, s.payMethod, s.addedPoints, s.cashierName, s.user.userCode, s.customer.customerCode) FROM Sale s")
+    List<SaleDTO> findAllSales();
+    @Query("SELECT MAX (s.orderCode) FROM Sale s")
+    String getLastOrderCode();
 }
