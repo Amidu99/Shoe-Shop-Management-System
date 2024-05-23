634ba8b42a20b70e4f23c12eab7a5009808a43c5
Amidu99 <amidushamika@gmail.com>
2024-05-24 01:00:11 +0530

    update DetailRepo

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/DetailRepo.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/DetailRepo.java
@@ -3,7 +3,9 @@ package lk.ijse.HelloShoesBE.repo;
 import lk.ijse.HelloShoesBE.dto.SaleInventoriesDTO;
 import lk.ijse.HelloShoesBE.entity.SaleInventories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Set;
 
@@ -12,4 +14,8 @@ public interface DetailRepo extends JpaRepository<SaleInventories, String> {
     Set<SaleInventoriesDTO> findAllByOrderCode(String orderCode);
     @Query("SELECT NEW lk.ijse.HelloShoesBE.dto.SaleInventoriesDTO(si.orderDetailCode, si.inventory.itemCode, si.orderCode, si.size, si.qty) FROM SaleInventories si WHERE si.orderCode IN :orderCodes")
     List<SaleInventoriesDTO> findAllByOrderCodes(List<String> orderCodes);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SaleInventories si WHERE si.orderCode = :orderCode")
+    void removeAllByOrderCode(String orderCode);
 }
