fa9db4af38b10939876365167571c8d610cb4c51
Amidu99 <amidushamika@gmail.com>
2024-05-26 17:15:12 +0530

    add urls.js

---
+++ HelloShoesFE/assets/js/urls.js
@@ -0,0 +1,8 @@
+export const AuthServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/auth';
+export const CustomerServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/customer';
+export const EmployeeServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/employee';
+export const InventoryServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/inventory';
+export const SaleServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/sale';
+export const StockServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/stock';
+export const SupplierServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/supplier';
+export const UserServiceUrl = 'http://localhost:8080/helloshoesbe/api/v1/user';
