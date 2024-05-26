207d5a4d6be0c386a41b52ad2f0a6bbf4822ca06
Amidu99 <amidushamika@gmail.com>
2024-05-26 12:59:33 +0530

    add customer model

---
+++ HelloShoesFE/model/Customer.js
@@ -0,0 +1,19 @@
+export class Customer {
+    constructor(customerCode, customerName, gender, joinDate, level, totalPoints, dob, addLine1, addLine2, addLine3, addLine4, addLine5, contactNo, email, rpDateTime) {
+        this.customerCode = customerCode;
+        this.customerName = customerName;
+        this.gender = gender;
+        this.joinDate = joinDate;
+        this.level = level;
+        this.totalPoints = totalPoints;
+        this.dob = dob;
+        this.addLine1 = addLine1;
+        this.addLine2 = addLine2;
+        this.addLine3 = addLine3;
+        this.addLine4 = addLine4;
+        this.addLine5 = addLine5;
+        this.contactNo = contactNo;
+        this.email = email;
+        this.rpDateTime = rpDateTime;
+    }
+}
