c0f69ffa291c12abe147399383982dadeba52726
Amidu99 <amidushamika@gmail.com>
2024-05-26 17:52:40 +0530

    update Supplier model

--- HelloShoesFE/model/Supplier.js
+++ HelloShoesFE/model/Supplier.js
@@ -1,3 +1,16 @@
 export class Supplier {
-
+    constructor(supplierCode, supplierName, category, addLine1, addLine2, addLine3, addLine4, addLine5, addLine6, contactNo1, contactNo2, email) {
+        this.supplierCode = supplierCode;
+        this.supplierName = supplierName;
+        this.category = category;
+        this.addLine1 = addLine1;
+        this.addLine2 = addLine2;
+        this.addLine3 = addLine3;
+        this.addLine4 = addLine4;
+        this.addLine5 = addLine5;
+        this.addLine6 = addLine6;
+        this.contactNo1 = contactNo1;
+        this.contactNo2 = contactNo2;
+        this.email = email;
+    }
 }
