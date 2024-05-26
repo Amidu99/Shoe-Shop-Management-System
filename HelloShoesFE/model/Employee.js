1065a47006ebeec8fb2be181d43d65116fab26cc
Amidu99 <amidushamika@gmail.com>
2024-05-26 15:48:16 +0530

    update employee model

--- HelloShoesFE/model/Employee.js
+++ HelloShoesFE/model/Employee.js
@@ -1,5 +1,23 @@
 export class Employee {
-    constructor(){
-
+    constructor(employeeCode, employeeName, employeePic, gender, status, designation, role, dob, joinDate, branch, addLine1, addLine2, addLine3, addLine4, addLine5, contactNo, email, guardian, guardianContactNo){
+        this.employeeCode = employeeCode;
+        this.employeeName = employeeName;
+        this.employeePic = employeePic;
+        this.gender = gender;
+        this.status = status;
+        this.designation = designation;
+        this.role = role;
+        this.dob = dob;
+        this.joinDate = joinDate;
+        this.branch = branch;
+        this.addLine1 = addLine1;
+        this.addLine2 = addLine2;
+        this.addLine3 = addLine3;
+        this.addLine4 = addLine4;
+        this.addLine5 = addLine5;
+        this.contactNo = contactNo;
+        this.email = email;
+        this.guardian = guardian;
+        this.guardianContactNo = guardianContactNo;
     }
 }
