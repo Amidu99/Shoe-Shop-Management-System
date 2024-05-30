9d04dcecb0bb0975fa4c29bdce1e4b5309011787
Amidu99 <amidushamika@gmail.com>
2024-05-30 14:59:12 +0530

    update SaleInventoriesDTO

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/dto/SaleInventoriesDTO.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/dto/SaleInventoriesDTO.java
@@ -3,6 +3,7 @@ package lk.ijse.HelloShoesBE.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.sql.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,13 +14,15 @@ public class SaleInventoriesDTO implements SuperDTO {
     private String itemDesc;
     private double itemPrice;
     private String orderCode;
+    private Date date;
     private int size;
     private int qty;
 
-    public SaleInventoriesDTO(String orderDetailCode, String itemCode, String orderCode, int size, int qty){
+    public SaleInventoriesDTO(String orderDetailCode, String itemCode, String orderCode, Date date, int size, int qty){
         this.orderDetailCode = orderDetailCode;
         this.itemCode = itemCode;
         this.orderCode = orderCode;
+        this.date = date;
         this.size = size;
         this.qty = qty;
     }
