11f730a15976a8f71e767cfefc9f6f106dcd74bd
Amidu99 <amidushamika@gmail.com>
2024-06-02 10:33:17 +0530

    update CustomerRepo

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/CustomerRepo.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/repo/CustomerRepo.java
@@ -3,10 +3,14 @@ package lk.ijse.HelloShoesBE.repo;
 import lk.ijse.HelloShoesBE.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface CustomerRepo extends JpaRepository<Customer, String> {
     boolean existsByCustomerCode(String customerCode);
     Customer getCustomerByCustomerCode(String customerCode);
     @Query("SELECT MAX (c.customerCode) FROM Customer c")
     String getLastCustomerCode();
+    @Query("SELECT c FROM Customer c WHERE FUNCTION('MONTH', c.dob) = :month AND FUNCTION('DAY', c.dob) = :day")
+    List<Customer> findByMonthAndDay(@Param("month") int month, @Param("day") int day);
 }
