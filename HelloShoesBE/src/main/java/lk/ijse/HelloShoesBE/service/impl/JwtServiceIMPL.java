edb48cc788122f7fc98e2b66b644feebdaad106c
Amidu99 <amidushamika@gmail.com>
2024-06-04 00:40:16 +0530

    finalize the backend

--- HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/impl/JwtServiceIMPL.java
+++ HelloShoesBE/src/main/java/lk/ijse/HelloShoesBE/service/impl/JwtServiceIMPL.java
@@ -44,7 +44,7 @@ public class JwtServiceIMPL implements JwtService {
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact()
                 +
                 " | "
